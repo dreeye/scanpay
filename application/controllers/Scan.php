bf4e6992e61f7fe79034b0f6e1a5c83a8500f57b
dreeye <i@mopo.me>
2016-05-09 14:05:40 +0800

    test xml

--- application/controllers/Scan.php
+++ application/controllers/Scan.php
@@ -50,7 +50,7 @@ class ScanController extends Core
             'openid'     => $openId,
             'total_fee'        => 1,
             'notify_url'       => 'http://scanpay.vzhen.com/order_notify', // 支付结果通知网址，如果不设置则会使用配置里的默认地址
-            'trade_type'       => 'NATIVE',
+            //'trade_type'       => 'NATIVE',
             // ...
         ];
 
