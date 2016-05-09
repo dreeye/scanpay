de747a5f0847c71cbd39d97c1ff1a9ed28434efe
dreeye <i@mopo.me>
2016-05-09 17:02:10 +0800

    test xml

--- application/controllers/Scan.php
+++ application/controllers/Scan.php
@@ -10,29 +10,38 @@ class ScanController extends Core
     public function init()
     {
         parent::init();
+        $this->scanMod = new ScanModel();
         
     }
 
-    public function create_itemAction()
+    public function create_productAction()
     {
+        $body = ( $this->_post['body'] ?? $this->Response->error('40016')) ? : $this->Response->error('40019');
+        $detail = ( $this->_post['detail'] ?? $this->Response->error('40016') ) ? : $this->Response->error('40020');
+        $total_fee = ( $this->_post['total_fee'] ?? $this->Response->error('40016') ) ? : $this->Response->error('40021');
         $payLib = new Pay();
         $productId = $this->Common->random_string('alnum', 32);
         $qrUrl = $payLib->createQrUrl($productId);
-        $data = [
-            'qr'=>$qrUrl,
-            'createTime'=>time(),
+        $product = [
+            'body' => $body,
+            'detail' => $detail,
+            'total_fee' => $total_fee,
+            'product_id' => $productId,
+            'qr_url' => $qrUrl,
+            'create_time' => time(),
+            'update_time' => time(),
+            
         ];
-        $this->Response->success($data);
+        if($this->scanMod->addProduct($product)) {
+            $data = [
+                'qr'=>$qrUrl,
+                'createTime'=>time(),
+            ];
+            $this->Response->success($data);
+
+        }
         
-        /*$from = ( $this->_post['from'] ?? $this->Response->error('40016')) ? : $this->Response->error('40019');
-        $to = ( $this->_post['to'] ?? $this->Response->error('40016') ) ? : $this->Response->error('40020');
-        $date = ( $this->_post['date'] ?? date('Y-m-d')) ? : date('Y-m-d');
-        if ( ! $this->Common->validateDate($date, 'Y-m-d') ) $this->Response->error('40022');
-        $flightData = $this->Trip->air($from, $to, $date); 
-        echo '<pre>';print_r($flightData);echo '</pre>';exit(); 
-        if ($flightData) {
-            $this->Response->success($flightData);
-        }*/
+        
         
     }
 
@@ -43,15 +52,29 @@ class ScanController extends Core
         $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
         $productId = strval($postObj->product_id);
         $openId = strval($postObj->openid);
+        $appId = strval($postObj->appid);
+
+        // 根据微信发来的product id 获取商品信息
+        if (!$productData = $this->scanMod->getProduct($productId)) {
+            error_log('product is no exit: product id ='.$productId);
+            exit();
+        }
+        // 根据微信发来的app id获取微信数据 
+        if ( ! $weData = $this->scanMod->getWechat($appId) ) {
+            error_log('wechat data is no exit: app id ='.$appId);
+            exit();
+        }
         $attributes = [
-            'body'             => 'iPad mini 16G 白色',
-            'detail'           => 'iPad mini 16G 白色',
-            'out_trade_no'     => $productId,
-            'openid'     => $openId,
-            'total_fee'        => 1,
+            'body'             => $productData['body'],
+            'detail'           => $productData['detail'],
+            'out_trade_no'     => $this->Common->random_string('alnum', 32),
+            'product_id'       => $productId,
+            'openid'           => $openId,
+            'total_fee'        => $productData['total_fee'],
             'notify_url'       => 'http://scanpay.vzhen.com/order_notify', // 支付结果通知网址，如果不设置则会使用配置里的默认地址
             'trade_type'       => 'NATIVE',
-            // ...
+            'appid'            => $appId,
+            'mch_id'           => $weData['mch_id'],
         ];
 
         $order = new Order($attributes);
@@ -59,26 +82,46 @@ class ScanController extends Core
         $result = $payLib->createOrder($order);
         error_log('DEBUG: '.$result);
         error_log('DEBUG: '.json_encode($attributes));
+        // 订单创建成功,开始支付
         if ($result->return_code == 'SUCCESS' && $result->result_code == 'SUCCESS'){
-            $reply = [
-                    'return_code' =>$result->return_code,
-                    // 'return_msg' =>$result->return_msg,
-                    'appid'       =>$_SERVER['APP_ID'],
-                    'mch_id'        => $_SERVER['MER_ID'],
-                    'nonce_str' =>$result->nonce_str,
-                    'prepay_id' => $result->prepay_id,
-                    'result_code' =>$result->result_code,
-                    // 'err_code_des' =>$result->err_code_des ?? '',
-            ];
-             $reply['sign'] = \EasyWeChat\Payment\generate_sign($reply, $_SERVER['KEY'], 'md5'); 
-            $xml = $this->Common->toXml($reply);
-            error_log('DEBUG order xml :'.$xml);
-            echo $xml;
+            $attributes['paid'] = 0;
+            $attributes['create_time'] = time();
+            $attributes['update_time'] = time();
+        
+            if ($this->scanMod->addOrder($attributes)) {
+                    $reply = [
+                            'return_code' =>$result->return_code,
+                            // 'return_msg' =>$result->return_msg,
+                            'appid'       =>$_SERVER['APP_ID'],
+                            'mch_id'        => $_SERVER['MER_ID'],
+                            'nonce_str' =>$result->nonce_str,
+                            'prepay_id' => $result->prepay_id,
+                            'result_code' =>$result->result_code,
+                            // 'err_code_des' =>$result->err_code_des ?? '',
+                    ];
+                    $reply['sign'] = \EasyWeChat\Payment\generate_sign($reply, $_SERVER['KEY'], 'md5'); 
+                    $xml = $this->Common->toXml($reply);
+                    error_log('DEBUG order xml :'.$xml);
+                    echo $xml;
+
+            }
         }
     }
 
     public function order_notify()
     {
+        $response = $app->payment->handleNotify(function($notify, $successful){
+            // 你的逻辑
+            error_log("DEBUG notify :".$notify);
+            if (!$this->scanMod->getOrder($notify->out_trade_no)) {
+                error_log("DEBUG notify order not exist:".$notify->out_trade_no);
+                return 'Order not exist';
+            }
+            if ($successful) {
+                $this->scanMod->updateOrderPaid($notify->out_trade_no);
+            } 
+            return true; // 或者错误消息
+        }); 
 
     }
 
--- application/models/Scan.php
+++ application/models/Scan.php
@@ -1,28 +1,85 @@
 <?php 
 
-class CtripModel extends Model {
+class ScanModel extends Model {
 
-    const TBL_CITY = 'city';
+    const TBL_PRODUCT = 'product';
+    const TBL_WECHAT = 'wechat';
+    const TBL_ORDER = 'order';
 
-    public function addCity($data)
+    public function addProduct($data)
     {
-        $this->_db->where('name', $data['name']);
-        if (!$this->_db->getOne(SELF::TBL_CITY)){
-            if ( ! $id = $this->_db->insert(self::TBL_CITY, $data) ) {
-                 error_log('insert city data error '. $this->_db->getLastError());
+        $this->_db->where('product_id', $data['product_id']);
+        if (!$this->_db->getOne(SELF::TBL_PRODUCT)){
+            if ( ! $id = $this->_db->insert(self::TBL_PRODUCT, $data) ) {
+                 error_log('insert product data error '. $this->_db->getLastError());
                  exit();
             }
         }
         return TRUE;
     }
 
-    public function getCity()
+    public function getProduct($productId)
     {
-        $data = $this->_db->get(SELF::TBL_CITY, null, ['name', 'code']);
+        $this->_db->where('product_id', $productId);
+        $data = $this->_db->get(SELF::TBL_PRODUCT);
         if(!$data) {
             return FALSE;
         } 
         return $data;
+
+    }
+
+    public function getWechat($appId)
+    {
+        $this->_db->where('app_id', $appId);
+        $data = $this->_db->get(SELF::TBL_WECHAT);
+        if(!$data) {
+            return FALSE;
+        } 
+        return $data;
+
+    }
+
+    public function addOrder($data)
+    {
+        $this->_db->where('out_trade_no', $data['out_trade_no']);
+        if (!$this->_db->getOne(SELF::TBL_ORDER)){
+            if ( ! $id = $this->_db->insert(self::TBL_ORDER, $data) ) {
+                 error_log('insert order data error '. $this->_db->getLastError());
+                 exit();
+            }
+        }
+        return TRUE;
+
+    }
+
+    public function getOrder($out_trade_no)
+    {
+        $this->_db->where('out_trade_no', $out_trade_no);
+        $data = $this->_db->get(SELF::TBL_ORDER);
+        if(!$data) {
+            return FALSE;
+        } 
+        return $data;
+
+    }
+
+    public function updateOrderPaid($out_trade_no)
+    {
+        $this->_db->where('out_trade_no', $out_trade_no);
+        $data = [
+            'paid' => 1,
+            'update_time' => time(),
+        ];
+        if ($this->_db->update(SELF::TBL_ORDER, $data) ) {
+            return True;
+        }
+        else
+        {
+             error_log('update order paid error'. $this->_db->getLastError());
+             exit();
+
+        }
 
     }
 
